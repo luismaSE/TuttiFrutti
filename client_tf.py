f9edf996439eb4ce4b17527b25da7f8f1cfcda00
luismaSE <lm.sarmiento@alumno.um.edu.ar>
2023-03-30 16:15:00 -0300

    conexiones en paralelo

--- client_tf.py
+++ client_tf.py
@@ -1,26 +1,37 @@
 #!/usr/bin/python3
-import socket, sys
-import click
+import socket, sys , click , pickle
 
 @click.command()
 @click.option('-h','host',default="localhost",help='')
+@click.option('-ipv4',is_flag=True, flag_value=True,help="Espicifiación para realizar la conexion mediante IPv4")
+@click.option('-ipv6',is_flag=True, flag_value=True,help="Espicifiación para realizar la conexion mediante IPv6")
 @click.option('-p','--port',default=5000,help="puerto del server donde a conectar")
-@click.option('-m','--match',default=None,help="Codigo de la partida a la que me quiero unir")
+@click.option('-m','--match',default= 0 ,help="Codigo de la partida a la que me quiero unir")
 @click.option('-r','--rounds',default=3,help="Cantidad de las rondas de la partida")
 @click.option('-c','--categories',default=3,help="Cantidad de categorias que tendrá la partida")
 @click.option('-s','--size',default=2,help="Cantidad de jugadores")
 
-def client(h,p,m,r,c,s):
-    s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
-    s.connect((h,int(p)))
+def client(host,ipv4,ipv6,port,match,rounds,categories,size):
+    if ipv4 == True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    else:
+        s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+    
+    s.connect((host,int(port)))
+    if  not match:
+        petition = pickle.dumps([rounds,categories,size])
+    else:
+        petition = pickle.dumps(match)
+    s.sendall(petition)
+    
 
     while True:
-        msg1 = input('> ')
-        s.send(msg1.encode('ascii'))
-        if msg1 == 'exit':
+        msg_in = pickle.loads(b'' + s.recv(4096))
+        print(msg_in)
+        msg_out = input('> ')
+        s.sendall(pickle.dumps(msg_out))
+        if msg_out == 'exit':
             break
-        msg2 = s.recv(1024).decode('ascii')
-        print(msg2)
     s.close()
     sys.exit()
     
--- server_tf.py
+++ server_tf.py
@@ -1,29 +1,60 @@
-import socket , click , multiprocessing
+import socket , click , multiprocessing , pickle , os , sys
+
+class Server:
+    
+    def __init__(self,host,port,backlog):
+        self.host = host
+        self.port = port
+        self.backlog = backlog
+        self.q = multiprocessing.Queue()   
+        self.set_socket()
+        self.serve()
+        
+    def set_socket(self):
+        self.sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 0)
+        self.sock.bind((self.host, self.port))
+        self.sock.listen(self.backlog)
+        print(f"Server listening on {self.host}:{self.port}")
+        
+    def serve(self):
+        
+        while True:
+            client, address = self.sock.accept()
+            print(f"Accepted connection from {address[0]}:{address[1]}")
+            proc = multiprocessing.Process(target=self.handle,args=(client,))
+            proc.start()
+        
+            
+            #usar multiprocessing para aceptar conexiones en paralelo 
+            #recibir los parametros del cliente y tomar una decision
+            #   segun los parametros que pase el cliente debe crear_partida()
+            #   o Asignar_jugador() a una partida ya creada
+        
+
+    def handle(self,client):
+        data = b'' + client.recv(4096)
+        pet = pickle.loads(data)
+        print(f"soy el nuevo proceso :D, mi PID es:{os.getpid()}")
+        while True:
+            client.sendall(pickle.dumps('Mensaje de prueba, mandame algo'))
+            resp = pickle.loads(b'' + client.recv(4096))
+            print(f"Me respondieron: {resp} :D")
+            if resp == 'bye':
+                break
+        client.close()
+        sys.exit()
 
 @click.command()
-@click.option('--host', '-h', default='', help='Host IP to bind server to. Default is all available interfaces.')
-@click.option('--port', '-p', default=8000, type=int, help='Port to bind server to. Default is 8000.')
+@click.option('--host', '-h', default='', help='')
+@click.option('--port', '-p', default=8000, type=int, help='')
+@click.option('--backlog', '-l', default=5, type=int, help='')
 
-
-def server(host, port):
-    backlog = 5
-    sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 0)
-    sock.bind((host, port))
-    sock.listen(backlog)
-    print(f"Server listening on {host}:{port}")
+def clic(host,port,backlog):
+    servidor = Server(host,port,backlog)
     
-    while True:
-        client, address = sock.accept()
-        print(f"Accepted connection from {address[0]}:{address[1]}")
-        
-        #usar multiprocessing para aceptar conexiones en paralelo
-        # recibir los parametros del cliente y tomar una decision
-        #   segun los parametros que pase el cliente debe crear_partida()
-        #   o Asignar_jugador() a una partida ya creada
-        
-        client.close()
+
 
 
 def crear_partida():
@@ -38,4 +69,4 @@ def asignar_jugador(): #ver si conviene que este aca, o que sea un metodo de la 
 
 
 if __name__ == '__main__':
-    server()
+    clic()
